d3e7989c34a1e85c1fcf0ffe32e4a24e0eaada40
Dmytro Minyaylo <dimon9660@gmail.com>
2025-05-16 17:58:18 +0300

    fixed the challenge super().validate()

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -84,7 +84,7 @@ class MovieSessionListSerializer(MovieSessionSerializer):
 
 class TicketSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
-        data = super().validate(attrs=attrs)
+        data = super().validate(attrs)
         Ticket.validate_ticket(
             attrs["row"],
             attrs["seat"],
